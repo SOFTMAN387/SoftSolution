b45357b79cbb93cb0908ca3e346823963f0ca431
Abhishek Kumar <103893512+abhisheklmak@users.noreply.github.com>
2023-02-28 18:59:08 +0700

    added footer

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import Header from '../Header/Header';
+import React from "react";
+import Footer from "../footer/Footer";
+import Header from "../Header/Header";
 
 const Home = () => {
-    return (
-        <>
-        <Header />
-        <h1>Hello,Mini React</h1>
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <h1>Hello,Mini React</h1>
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
