08d92c8721f761d8137434cf19a18f5a15abf895
Manish Kumar Gupta <manishkumarpatna2020@gmail.com>
2023-02-28 17:14:51 +0530

    IT Solutions

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Header/Header';
+
+const Home = () => {
+    return (
+        <>
+        <Header />
+        <h1>Hello,Mini React</h1>
+        </>
+    )
+}
+
+export default Home;
