47a62cac9067607b059eb0cac0ac66f806b8300a
Manish Kumar Gupta <manishkumarpatna2020@gmail.com>
2023-03-02 11:41:48 +0530

    Updated

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import Header from '../Header/Header';
+//import Header from '../Header/Header';
+import SideBar from '../sidebar/SideBar';
 
 const Home = () => {
     return (
         <>
-        <Header />
+        {/* <Header /> */}
         <h1>Hello,Mini React</h1>
+        <SideBar />
         </>
     )
 }
